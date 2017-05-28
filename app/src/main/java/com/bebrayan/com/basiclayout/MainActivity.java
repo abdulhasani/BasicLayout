4254005d6a883eaa7997f44a236ce85c00817421
BHasani <abdul.kadir.hasani@basajans.com>
2017-05-28 23:17:29 +0700

    example form login with layout TableLayout

--- app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_layout);
     }
 }
