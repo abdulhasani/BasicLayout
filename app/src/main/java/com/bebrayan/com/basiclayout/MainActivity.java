49fa66761b3e43feabf1abc32201318a6d43705e
BHasani <abdul.kadir.hasani@basajans.com>
2017-05-28 23:38:44 +0700

    example use FrameLayout

--- app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
@@ -8,5 +8,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.example_frame_layout);
     }
 }
