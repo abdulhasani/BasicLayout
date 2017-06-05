ea1f141ec1a84f54889add239bc44ffbd9db33f8
BHasani <abdul.kadir.hasani@basajans.com>
2017-06-06 00:35:25 +0700

    create UI

--- app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
+++ app/src/main/java/com/bebrayan/com/basiclayout/MainActivity.java
@@ -2,11 +2,19 @@ package com.bebrayan.com.basiclayout;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         ImageView header = (ImageView) findViewById(R.id.header);
+        Glide.with(this).load(R.drawable.singapore).diskCacheStrategy(DiskCacheStrategy.ALL).centerCrop().into(header);
+
     }
 }
